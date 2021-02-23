8f156e741addcbb0e7795c3aa411f9d42cd7f68b
dongwon <kkum9408@gmail.com>
2021-02-23 20:11:30 +0900

    Set public url in github action

--- config/paths.js
+++ config/paths.js
@@ -15,11 +15,29 @@ const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 // single-page apps that may serve index.html for nested URLs like /todos/42.
 // We can't use a relative path in HTML because we don't want to load something
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
+
+// start : deployment
+// build : production
+
 const publicUrlOrPath = getPublicUrlOrPath(
   process.env.NODE_ENV === 'development',
   require(resolveApp('package.json')).homepage,
   process.env.PUBLIC_URL,
 );
+
+// const publicUrlOrPath =
+//   process.env.NODE_ENV === 'development'
+//     ? getPublicUrlOrPath(
+//         process.env.NODE_ENV === 'development',
+//         // require(resolveApp('package.json')).homepage,
+//         '/',
+//         process.env.PUBLIC_URL,
+//       )
+//     : getPublicUrlOrPath(
+//         process.env.NODE_ENV === 'development',
+//         require(resolveApp('package.json')).homepage,
+//         process.env.PUBLIC_URL,
+//       );
 
 const moduleFileExtensions = [
   'web.mjs',
